13d4b9636a04c40ff83a60f09641dd3ea5e8f74e
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-07-19 11:37:59 -0300

    Rotas e Páginas Dinâmicas

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #primeiro importar flask
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for  #url_for permite alterar rota sem ter problemas
 # render_template ,faz carregar os arquivos html, na pasta templates
 
 app = Flask(__name__)
 
 #criar rota(caminho da pagina)da pagina homepage
-@app.route("/")
+@app.route("/homepage")
 def homepage():
     return render_template("homepage.html")
 
-#criar rota(caminho da pagina)da pagina perfil
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
+#criar rota(caminho da pagina)da pagina perfil, para qualquer usuario
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
 
 
 
