fe8e772dbad10080c78bed61e18a786e7f330b7f
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-07-19 11:01:01 -0300

    Usando templates html nas nossas páginas

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #primeiro importar flask
-from flask import Flask
+from flask import Flask, render_template
+# render_template ,faz carregar os arquivos html, na pasta templates
 
 app = Flask(__name__)
 
-#criar rota(caminho do site)
+#criar rota(caminho da pagina)da pagina homepage
 @app.route("/")
 def homepage():
-    return "fakePinterest - Meu primeiro site no ar"
+    return render_template("homepage.html")
+
+#criar rota(caminho da pagina)da pagina perfil
+@app.route("/perfil")
+def perfil():
+    return "Perfil do Usuário"
+
+
 
 #se for importado de outro arquivo, não é para rodar esse codigo
 if __name__ == "__main__":
