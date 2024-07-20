7e14725e2c6d84c08f00ebbd606e9044e28e63af
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-07-20 11:04:32 -0300

    Criar formulários de login e de criar conta

--- fakePinterest/forms.py
+++ fakePinterest/forms.py
@@ -1,1 +1,29 @@
 #criar formulários do site
+#pip install flask-wtf
+#pip install email-validator
+
+from flask_wtf import FlaskForm  #estrutura da classe
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakePinterest.models import Usuario
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de Usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmacao de senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario: #se ja existe usuario com este email
+            return ValidationError("E-mail já cadastrado, faça login para continuar.")
+
