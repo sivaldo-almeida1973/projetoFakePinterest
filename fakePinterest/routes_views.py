d36aaa2be90dbf8ca18acbe7a85dc36c04cc087b
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-07-20 21:16:20 -0300

    Implementar o formulário de login na homepage e de criar conta

--- fakePinterest/routes_views.py
+++ fakePinterest/routes_views.py
@@ -1,13 +1,20 @@
 #criar rotas do nosso site(links)
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 #import o app que esta dentro do __init__ para poder funcionar
 from fakePinterest import app
 from flask_login import login_required
+from fakePinterest.forms import FormLogin, FormCriarConta
 
 #criar rota(caminho da pagina)da pagina homepage
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
+
+@app.route("/criarconta",  methods=["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form=formcriarconta)
 
 #criar rota(caminho da pagina) perfil, para qualquer usuario
 @app.route("/perfil/<usuario>")
